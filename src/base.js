361e4bd100fd87de45cd5c0e9aa921de742f42dc
jesus.salinas <Jesus.Salinas@tescobank.com>
2018-12-18 13:38:37 +0000

    Removing API KEY

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyCKprueGVW0HWXfW-G44d_-0th3j2mWUrE",
+        apiKey: "secret",
         authDomain: "catch-the-fish-9cb31.firebaseapp.com",
         databaseURL: "https://catch-the-fish-9cb31.firebaseio.com"
 });
