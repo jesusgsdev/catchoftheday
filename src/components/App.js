806f0dc1f1decdbf67d549c5328d77f75038a276
jesus.salinas <Jesus.Salinas@tescobank.com>
2018-12-18 14:04:25 +0000

    Adding functionality to remove fishes form menu and from the order

--- src/components/App.js
+++ src/components/App.js
@@ -17,21 +17,24 @@ class App extends React.Component {
     const { params } = this.props.match;
     //First reinstate our localStorage
     const localStorageRef = localStorage.getItem(params.storeId);
-    if(localStorageRef) {
-      this.setState({ order : JSON.parse(localStorageRef)});
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
     }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
-        context: this, 
-        state: 'fishes'
+      context: this,
+      state: "fishes"
     });
   }
 
-  componentDidUpdate(){
-      localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
   }
 
   componentWillUnmount() {
-      base.removeBinding(this.ref);
+    base.removeBinding(this.ref);
   }
 
   addFish = fish => {
@@ -45,14 +48,23 @@ class App extends React.Component {
     });
   };
 
-updateFish = (key, updatedFish) => {
-  //1. Take a copy of the current state
-  const fishes = { ...this.state.fishes };
-  //2. Update that state
-  fishes[key] = updatedFish;
-  //3. Set that to state
-  this.setState({ fishes });
-}
+  updateFish = (key, updatedFish) => {
+    //1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    //2. Update that state
+    fishes[key] = updatedFish;
+    //3. Set that to state
+    this.setState({ fishes });
+  };
+
+  deleteFish = key => {
+    //1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    //2. Update that state, we need to set it to null to be removed from Firebase
+    fishes[key] = null;
+    //3. Set that to state
+    this.setState({ fishes });
+  };
 
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
@@ -63,6 +75,15 @@ updateFish = (key, updatedFish) => {
     const order = { ...this.state.order };
     //2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
+    //3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = key => {
+    //1. Take a copy of state
+    const order = { ...this.state.order };
+    //2. As we are not mirroring on Firebase, we can use delete keyword
+    delete order[key];
     //3. Call setState to update our state object
     this.setState({ order });
   };
@@ -83,10 +104,15 @@ updateFish = (key, updatedFish) => {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
